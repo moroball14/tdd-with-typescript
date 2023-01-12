ad3e9c035d2eef7e2ca04e058f3e8462fab01ed7
moroball14 <daiki.mr.0330@gmail.com>
2023-01-13 06:07:36 +0900

    chapter6: If you notice a lack of testing

--- src/money.ts
+++ src/money.ts
@@ -1,14 +1,9 @@
-export class Dollar {
-  private amount: number;
+export abstract class Money {
+  protected amount: number;
   constructor(amount: number) {
     this.amount = amount;
   }
-
-  public times = (multiplier: number): Dollar => {
-    return new Dollar(this.amount * multiplier);
-  };
-
-  public equals = (dollar: Dollar): boolean => {
+  public equals = (dollar: Money): boolean => {
     return this.amount === dollar.amount;
   };
 }
