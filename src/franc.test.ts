a006600e4caa96e774daab1b5fdc52b12f0339ba
moroball14 <daiki.mr.0330@gmail.com>
2023-01-14 06:46:11 +0900

    chapter8: Hide implementation

--- src/franc.test.ts
+++ src/franc.test.ts
@@ -1,16 +1,15 @@
-import { Dollar } from './dollar';
 import { Franc } from './franc';
 
 describe('FrancTest', () => {
   it('multiplication', () => {
-    const five: Franc = new Franc(5);
-    expect(five.times(2).equals(new Franc(10))).toBe(true);
-    expect(five.times(3).equals(new Franc(15))).toBe(true);
+    const five: Franc = Franc.createInstance(5);
+    expect(five.times(2).equals(Franc.createInstance(10))).toBe(true);
+    expect(five.times(3).equals(Franc.createInstance(15))).toBe(true);
     // クラス同士の比較は、equalsメソッドを経由して行う必要がある
     // ref: https://jestjs.io/docs/expect#toequalvalue
   });
   it('equals', () => {
-    expect(new Franc(5).equals(new Franc(5))).toBe(true);
-    expect(new Franc(5).equals(new Franc(6))).toBe(false);
+    expect(Franc.createInstance(5).equals(Franc.createInstance(5))).toBe(true);
+    expect(Franc.createInstance(5).equals(Franc.createInstance(6))).toBe(false);
   });
 });
