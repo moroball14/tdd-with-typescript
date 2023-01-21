a44890eb1e35e009d6c3db6543e32a86213e7182
moroball14 <daiki.mr.0330@gmail.com>
2023-01-21 09:06:55 +0900

    chapter11: Delete when no longer needed

--- src/money.test.ts
+++ src/money.test.ts
@@ -1,16 +1,21 @@
-import { Dollar } from './dollar';
-import { Franc } from './franc';
 import { Money } from './money';
 
 describe('MoneyTest', () => {
+  it('multiplication', () => {
+    const five: Money = Money.dollar(5);
+    expect(five.times(2).equals(Money.dollar(10))).toBe(true);
+    expect(five.times(3).equals(Money.dollar(15))).toBe(true);
+    // クラス同士の比較は、equalsメソッドを経由して行う必要がある
+    // ref: https://jestjs.io/docs/expect#toequalvalue
+  });
   it('equals', () => {
-    expect(Franc.createInstance(5).equals(Dollar.createInstance(5))).toBe(
-      false
-    );
-    expect(Franc.createInstance(10).equals(new Money(10, 'CHF'))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
+    // expect(Money.franc(10).equals(new Money(10, 'CHF'))).toBe(true);
   });
   it('currency', () => {
-    expect(Dollar.createInstance(1).getCurrency()).toBe('USD');
-    expect(Franc.createInstance(1).getCurrency()).toBe('CHF');
+    expect(Money.dollar(1).getCurrency()).toBe('USD');
+    expect(Money.franc(1).getCurrency()).toBe('CHF');
   });
 });
